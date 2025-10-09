59a2179d164da155f4dfc72deaa442f993c6a11b
GESS0407 <04gess@gmail.com>
2025-10-09 10:05:02 -0300

    Teste

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+const email = document.getElementById("email");
+const senha = document.getElementById("password");
+const btnEntrar = document.getElementById("btnEntrar");
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (email.value === "Gui" && senha.value === "123") {
+    window.location.href = "./index.html";
+  }
+});
